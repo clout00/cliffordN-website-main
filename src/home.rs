a4781237f1a5fbbc4bf196f1470d253692028408
Clifford <cnkarenbi@gmail.com>
2023-02-16 13:53:01 +0200

    gg

---
+++ src/home.rs
@@ -0,0 +1,21 @@
+use yew::prelude::*;
+
+#[function_component]
+pub fn App() -> Html {
+    html! {
+        <div class="container">
+        <div id="navigation-bar">
+          <nav>
+            <ul>
+              <li><img src="res/pplogo.png" alt="logo" /></li>
+              
+            </ul>
+          </nav>
+        </div>
+      
+        <div class="content">
+          
+        </div>
+      </div>
+   }
+}
